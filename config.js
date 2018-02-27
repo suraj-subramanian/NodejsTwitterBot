a807ff70a4737ba90c105a6301849a4be41eb7cb
Suraj Subramanian <suraj.tf6@gmail.com>
2018-02-27 19:17:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports={
-    consumer_key:         'snOByfP1N1b8jOvxzEt3m05WD',
-    consumer_secret:      '78esNeKsSfMvAMmiJEs8y16BaiFn6BJVK4AFgNMiuQbionGWsW',
-    access_token:         '965503361410768897-gSr1X0fXZ9L0pLiCjeq0xe2MM9kqXUq',
-    access_token_secret:  'T0ykrErHuJECH07EyMBR5TvqRybBF1bpHJcFkrDF8EyGf',
+    consumer_key:         '...',   // use the API keys for the twitter account you want to use.
+    consumer_secret:      '...',
+    access_token:         '...',
+    access_token_secret:  '...', 
     timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 }
